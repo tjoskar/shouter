fa915092a83de9412b15a2530f0c2e35e1b296f1
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-07-19 22:05:15 +0200

    Add main index file

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _shouter = require('./dist/index');
+
+exports.shouter = _shouter.shouter;
+exports.triggerOnEvent = _shouter.triggerOnEvent;
+exports.shoutOnSet = _shouter.shoutOnSet;
+exports.shoutOnGet = _shouter.shoutOnGet;
+exports.default = _shouter.shouter;
