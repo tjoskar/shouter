f5011a726559cd8ea5c0a762a7bc459435b45836
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-08-10 13:08:31 +0200

    New Build

---
+++ dist/browser.js
@@ -0,0 +1,2 @@
+"use strict";var _shouter=require("./shouter");if("undefined"!=typeof window)window.shouter=_shouter.shouter;else{var glob=new Function("return this")();glob.shouter=_shouter.shouter}
+//# sourceMappingURL=browser.js.map
