b07f215c587e0fcb2f767a09594a81901fff1867
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2015-11-21 10:46:13 +0100

    New build

--- dist/lib/global.js
+++ dist/lib/global.js
@@ -1,2 +1,2 @@
 "use strict";Object.defineProperty(exports,"__esModule",{value:!0});var globalObject=function(){return"undefined"!=typeof self?self:"undefined"!=typeof global?global:"undefined"!=typeof window?window:new Function("return this")()}();exports.globalObject=globalObject;
-//# sourceMappingURL=../lib/global.js.map
+//# sourceMappingURL=global.js.map
