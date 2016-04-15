6f5099979cce6b28a4b1d41ed298ab577c440a81
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2016-04-15 17:35:18 +0200

    fix(dist) remove dist folder

--- dist/browser.js
+++
@@ -1,2 +0,0 @@
-"use strict";var _shouter=require("./shouter");if("undefined"!=typeof window)window.shouter=_shouter.shouter;else{var glob=new Function("return this")();glob.shouter=_shouter.shouter}
-//# sourceMappingURL=browser.js.map
--- dist/lib/global.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var globalObject=function(){return"undefined"!=typeof self?self:"undefined"!=typeof global?global:"undefined"!=typeof window?window:new Function("return this")()}();exports.globalObject=globalObject;
-//# sourceMappingURL=global.js.map
